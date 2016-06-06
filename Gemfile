73f2258c78dc61e80d3b5e4f8442e4835e354c99
straysheep3 <getloose.kage@gmail.com>
2016-06-07 02:00:59 +0900

    paperclip設定完了

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'bootstrap-sass', '~> 3.3', '>= 3.3.6'
 gem 'simple_form', '~> 3.2', '>= 3.2.1'
 gem 'devise', '~> 4.1', '>= 4.1.1'
+gem 'paperclip', '~> 4.3', '>= 4.3.6'
 
 group :development, :test do
   gem 'byebug'
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :rooms
+
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "25x25>" }
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 end
